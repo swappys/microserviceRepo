8f2d2369f6221dd11aa765d68b45f03b04166cfc
swapnil <swapnilvernekar04@gmail.com>
2024-01-15 22:36:16 +0000

    updated application.yaml and created api for order service

---
+++ OrderService/OrderService/src/main/java/com/example/OrderService/model/OrderRequest.java
@@ -0,0 +1,17 @@
+package com.example.OrderService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderRequest {
+    private long productId;
+    private long totalAmount;
+    private long quantity;
+    private PaymentMode paymentMode;
+}
